f6d7e7893dea1fcd30e1f3211a2804400a2fe0a9
R. A. Souza <alves.wm@gmail.com>
2014-04-22 20:45:02 -0300

    Standardized C code

--- src/lib/circularbuffer.c
+++ src/lib/circularbuffer.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-// #include <stdio.h>
+#include <stdio.h>
 #include "circularbuffer.h"
 
 union CBData;
@@ -37,28 +37,28 @@ int CB_isEmpty(CircularBuffer *cb)
     return cb->end == cb->start;
 }
 
-/////////////
-// EXEMPLO //
-/////////////
-
-// int main(int argc, char const *argv[])
-// {
-//     CircularBuffer cb;
-//     CBData elem = {0};
-//     int bufferSize = 10;
-
-//     CB_init(&cb,bufferSize);
-
-//     for (elem.value = 0; elem.value < 3 * bufferSize; ++elem.value)
-//         CB_write(&cb, &elem);
+/*
+EXEMPLO
 
 
-//     while (!CB_isEmpty(&cb)) {
-//         CB_read(&cb, &elem);
-//         printf("%d\n", elem.value);
-//     }
+int main(int argc, char const *argv[])
+{
+    CircularBuffer cb;
+    CBData elem = {0};
+    int bufferSize = 10;
 
-//     CB_free(&cb);
+    CB_init(&cb,bufferSize);
 
-//     return 0;
-// }
+    for (elem.value = 0; elem.value < 3 * bufferSize; ++elem.value)
+        CB_write(&cb, &elem);
+
+    while (!CB_isEmpty(&cb)) {
+       CB_read(&cb, &elem);
+        printf("%d\n", elem.value);
+    }
+
+    CB_free(&cb);
+
+    return 0;
+}
+*/
--- src/lib/position.c
+++ src/lib/position.c
@@ -1,3 +1,4 @@
+#include "position.h"
 /**
  * Basico para o sistema de posicionamento
  */
