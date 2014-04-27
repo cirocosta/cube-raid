a9ca0e673471bf5aa8fb749d2b561734031620e8
Ciro S. Costa <ciro.costa@usp.br>
2014-04-27 18:17:08 -0300

    fixes #8

--- src/maps/map1.c
+++ src/maps/map1.c
@@ -5,11 +5,8 @@ Queue generate_map()
 	Queue map1 = queueInit();
 	CBData *elemento = malloc(sizeof *elemento);
 
-	elemento->nave = NAVE_create(POS_create(0, 0, 0), 10, 10, 10);
+	elemento->nave = NAVE_create(POS_create(0, 0, 0), 10, POS_create(10, 10, 10), 10);
 	queuePut(map1, *elemento);
-
-
-
 
 	return map1;
 }
